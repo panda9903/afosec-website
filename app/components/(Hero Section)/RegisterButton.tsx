ff5abc47b5c51b26f4221c12de23d9f95330133d
panda9903 <satwik9903@gmail.com>
2024-01-20 07:45:46 +0530

    Changed styles

--- app/components/(Hero Section)/RegisterButton.tsx
+++ app/components/(Hero Section)/RegisterButton.tsx
@@ -7,7 +7,9 @@ import { Button } from "@/components/ui/button";
 export function RegisterButton() {
   return (
     <Button asChild className="mx-auto px-8 bg-[#EA8814] text-white">
-      <Link href="/register">Register</Link>
+      <Link href="/register">
+        <span className="text-lg">Register</span>
+      </Link>
     </Button>
   );
 }
