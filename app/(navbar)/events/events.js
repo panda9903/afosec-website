c6b1bcd514ae0e9d86cae05f0522f0a54633c571
panda9903 <satwik9903@gmail.com>
2024-01-26 21:43:47 +0530

    Added events

---
+++ app/(navbar)/events/events.js
@@ -0,0 +1,186 @@
+const events = {
+  CSE: [
+    {
+      id: 1,
+      name: "Secure Knight",
+      description: "Description about event",
+    },
+    {
+      id: 2,
+      name: "Shark Tank",
+      description: "Description about event",
+    },
+    {
+      id: 3,
+      name: "Trilang Quest",
+      description: "Description about event",
+    },
+    {
+      id: 4,
+      name: "Rapid ML",
+      description: "Description about event",
+    },
+  ],
+  AIDS: [
+    {
+      id: 1,
+      name: " Green Gyan",
+      description: "Description about event",
+    },
+    {
+      id: 2,
+      name: "Code Wizards",
+      description: "Description about event",
+    },
+  ],
+  IT: [
+    {
+      id: 1,
+      name: "UI Utopia / PixelFiesta",
+      description: "Description about event",
+    },
+    {
+      id: 2,
+      name: "Argumentation",
+      description: "Description about event",
+    },
+    {
+      id: 3,
+      name: "Fast&Furious",
+      description: "Description about event",
+    },
+    {
+      id: 4,
+      name: "Bug Busters",
+      description: "Description about event",
+    },
+  ],
+  ECE: [
+    {
+      id: 1,
+      name: "Technoslide",
+      description: "Description about event",
+    },
+    {
+      id: 2,
+      name: "Electronic Circuit Quiz",
+      description: "Description about event",
+    },
+    {
+      id: 3,
+      name: "Electronic Project Exhibition",
+      description: "Description about event",
+    },
+    {
+      id: 4,
+      name: "Techno Debate",
+      description: "Description about event",
+    },
+  ],
+  EEE: [
+    {
+      name: "Tech Broadcast",
+      description: "Description about event",
+    },
+    {
+      name: "Solder Master",
+      description: "Description about event",
+    },
+    {
+      name: "D-Tech-Tive",
+      description: "Description about event",
+    },
+    {
+      name: "tech Debate",
+      description: "Description about event",
+    },
+  ],
+  Mech: [
+    {
+      name: "EngineX",
+      description: "Description about event",
+    },
+    {
+      name: "Lathe War",
+      description: "Description about event",
+    },
+    {
+      name: "Water Rocket Lavunch",
+      description: "Description about event",
+    },
+    {
+      name: "Rube Gold Berg Maachine",
+      description: "Description about event",
+    },
+  ],
+  Civil: [
+    {
+      name: "Bridge It",
+      description: "Description about event",
+    },
+    {
+      name: "Seismic Table",
+      description: "Description about event",
+    },
+    {
+      name: "Auto Cad",
+      description: "Description about event",
+    },
+    {
+      name: "E-Poster Presentation",
+      description: "Description about event",
+    },
+  ],
+  MBA: [
+    {
+      name: "Brain Blasters",
+      description: "Description about event",
+    },
+    {
+      name: "Waall Street Wizards",
+      description: "Description about event",
+    },
+    {
+      name: "Market Mavericks",
+      description: "Description about event",
+    },
+    {
+      name: "Workforce Hives",
+      description: "Description about event",
+    },
+  ],
+  MCA: [
+    {
+      name: "Aakritiparva",
+      description: "Description about event",
+    },
+    {
+      name: "Techno Vidya",
+      description: "Description about event",
+    },
+    {
+      name: "Avishkar Anveshan",
+      description: "Description about event",
+    },
+  ],
+  EIE: [
+    {
+      name: "Kushagra",
+      description: "Description about event",
+    },
+    {
+      name: "PCB Design",
+      description: "Description about event",
+    },
+    {
+      name: "Techno Cross Word",
+      description: "Description about event",
+    },
+    {
+      name: "Tech-Talk",
+      description: "Description about event",
+    },
+  ],
+};
+
+export default events;
