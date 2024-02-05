d7844214ee934ae03babe2065d0f36eb0de863a1
chanakya <218w1a05c1@vrsec.ac.in>
2024-02-05 22:41:48 +0530

    Update social media links in Footer component

--- app/components/(Footer)/Links.tsx
+++ app/components/(Footer)/Links.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 import Image from "next/image";
 import Instagram from "@public/Group.svg";
 import Facebook from "@public/Group (1).svg";
+import YouTube from "@public/youtube.svg";
 import Twitter from "@public/Group (2).svg";
 import Linkedin from "@public/Vector.svg";
 
 const Links = () => {
   return (
     <div className="flex justify-stretch">
-      <a href="https://www.instagram.com" target="_blank">
+      <a href="https://www.instagram.com/afosec_vrsec" target="_blank">
         <Image
           src={Instagram}
           height={30}
@@ -18,7 +19,7 @@ const Links = () => {
           className="my-4 mr-2"
         ></Image>
       </a>
-      <a href="https://www.facebook.com" target="_blank">
+      <a href=" https://www.facebook.com/profile.php?id=61554640401260&mibextid=LQQJ4d " target="_blank">
         <Image
           src={Facebook}
           height={30}
@@ -27,7 +28,16 @@ const Links = () => {
           className="my-4 mx-2"
         ></Image>
       </a>
-      <a href="https://www.twitter.com" target="_blank">
+      <a href=" https://www.youtube.com/@afosecVrsec" target="_blank">
+        <Image
+          src={YouTube}
+          height={30}
+          width={30}
+          alt="Logo"
+          className="my-4 mx-2"
+        ></Image>
+      </a>
+      <a href=" https://x.com/AFOSECVRSEC " target="_blank">
         <Image
           src={Twitter}
           height={30}
@@ -36,7 +46,7 @@ const Links = () => {
           className="my-4 mx-2"
         ></Image>
       </a>
-      <a href="https://www.linkedin.com" target="_blank">
+      <a href=" https://www.linkedin.com/in/afosec-vrsec-2k24 " target="_blank">
         <Image
           src={Linkedin}
           height={30}
