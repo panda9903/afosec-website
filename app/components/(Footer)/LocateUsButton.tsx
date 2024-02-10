c00787888aa6a666dbfc4230e653b4e9f3c82509
chanakya <218w1a05c1@vrsec.ac.in>
2024-02-10 09:52:40 +0530

    Update LocateUsButton styling

--- app/components/(Footer)/LocateUsButton.tsx
+++ app/components/(Footer)/LocateUsButton.tsx
@@ -10,7 +10,7 @@ export function LocateUsButton() {
     <div className="mx-auto">
       <Button
         asChild
-        className="px-8 bg-[#FFFFFF] text-black"
+        className="px-10 bg-[#FFFFFF] text-black hover:bg-[#EBD80D]"
       >
         <Link href="https://maps.app.goo.gl/AdbMzj3PVDABnzxa8?g_st=ic">
           <span className="text-lg">Locate Us</span>
