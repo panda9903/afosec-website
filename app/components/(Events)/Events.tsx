4d518eb590793f7ee728d60bde31acba6ffdc9d3
Chanikya5793 <148619135+Chanikya5793@users.noreply.github.com>
2024-02-10 17:08:09 +0530

    Update dependencies and remove unused code

--- app/components/(Events)/Events.tsx
+++ app/components/(Events)/Events.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import EventCarousel from "./EventCarousel";
-import Doodle1 from "@public/BalletDoodle.svg";
 import IdeationCarousel from "./IdeationCarousel";
-import Ideation from "@public/EmpInd.svg";
 const Events = () => {
   return (
     <section id="events">
       <h2 className="text-3xl font-bold text-center mt-20 mb-10">Central Events</h2>
       <div className="relative flex flex-col md:flex-row justify-around items-start">
         <div className="absolute z-0 top-0 left-0 w-full h-full flex items-center justify-center">
-          <Image
-            src={Doodle1}
+        {/*  <Image
+            src={}
             alt="events"
             width={620}
             height={620}
           />
+  */}
           
         </div>
 
