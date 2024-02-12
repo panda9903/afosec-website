b3becd7549ce358ffcd91dc595f76ba49764a0fd
panda9903 <satwik9903@gmail.com>
2024-02-12 12:15:49 +0530

    Modal Boxes for Events

--- app/(navbar)/events/EventList.tsx
+++ app/(navbar)/events/EventList.tsx
@@ -6,16 +6,118 @@ import { useRouter } from "next/navigation";
 import "./styles.css";
 import { Button } from "@/components/ui/button";
 
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+
+import ReactDOM from "react-dom";
+
+type Event = {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  deg: number;
+  from: string;
+  to: string;
+};
+
 const EventsList = () => {
   const events = Object.entries(eventList);
   const router = useRouter();
 
-  const handleClick = (name: string) => {
-    router.push(`/events/${name}`);
+  const [show, setShow] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
+
+  const handleClick = (event: any) => {
+    setSelectedEvent(event);
+    handleShow();
   };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
+      {selectedEvent && (
+        <Modal
+          show={show}
+          onHide={handleClose}
+          centered
+          className=" overflow-x-auto overflow-y-scroll  fixed md:left-1/4 md:right-1/4 top-1/4 left-[10%] right-[10%] bottom-8 backdrop-blur-3xl bg-gray-900 p-6
+        "
+        >
+          <Modal.Header closeButton className="flex flex-row justify-between">
+            <Modal.Title className="text-white text-3xl mb-8 ">
+              {selectedEvent.name}
+            </Modal.Title>
+            <Button
+              onClick={handleClose}
+              className="p-2 h-8 w-8 rounded-full bg-white text-black hover:bg-red-600 md:absolute md:right-8"
+            >
+              X
+            </Button>
+          </Modal.Header>
+          <Modal.Body className="text-white text-base">
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+            {selectedEvent.description}
+          </Modal.Body>
+          <Modal.Footer className="mt-8">
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
       {events.map(([department, events]) => {
         return (
           <div className="mb-8 flex flex-col" key={department}>
@@ -46,7 +148,15 @@ const EventsList = () => {
                           <Button
                             className="bg-[#EA8814] mt-4"
                             onClick={() => {
-                              handleClick(url);
+                              handleClick({
+                                id,
+                                name,
+                                description,
+                                url,
+                                deg,
+                                from,
+                                to,
+                              });
                             }}
                           >
                             Know More
@@ -64,5 +174,4 @@ const EventsList = () => {
     </>
   );
 };
-
 export default EventsList;
