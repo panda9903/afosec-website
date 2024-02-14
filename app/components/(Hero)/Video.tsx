e6da32accdc2eda416e0b2b18b61fc20ad3d9fca
Chanikya5793 <148619135+Chanikya5793@users.noreply.github.com>
2024-02-14 08:12:31 +0000

    Refactor video component to improve responsiveness

--- app/components/(Hero)/Video.tsx
+++ app/components/(Hero)/Video.tsx
@@ -1,41 +1,40 @@
 "use client";
-import React, { useEffect } from "react";
-import { useState } from "react";
-import {
-  useWindowWidth,
-} from '@react-hook/window-size'
+import React from "react";
+import { useWindowWidth } from '@react-hook/window-size'
 
 const Video = () => {
   const width = useWindowWidth()
-  
-  const isDesktop = width > 768;
-
-  //alert(isDesktop)
+  const isDesktop = width > 720;
 
   return (
     <div className="mb-4">
-      <video
-        autoPlay
-        controls={false}
-        muted
-        playsInline
-        preload="true"
-        loop
-        className="w-screen h-screen object-cover"
-      >
-        {/*{width > 768 ? ( 
+      {isDesktop ? (
+        <video
+          autoPlay
+          controls={false}
+          muted
+          playsInline
+          preload="true"
+          loop
+          className="w-screen h-screen object-cover"
+        >
           <source src="../../../landscape.mp4" type="video/mp4" />
-        ) : (
+          Your browser does not support HTML5 video.
+        </video>
+      ) : (
+        <video
+          autoPlay
+          controls={false}
+          muted
+          playsInline
+          preload="true"
+          loop
+          className="w-screen h-screen object-cover"
+        >
           <source src="../../../potrait.mp4" type="video/mp4" />
-        )}*/}
-        {
-          isDesktop && <source src="../../../landscape.mp4" type="video/mp4" />
-        }
-        {
-          !isDesktop && <source src="../../../potrait.mp4" type="video/mp4" />
-        }
-        Your browser does not support HTML5 video.
-      </video>
+          Your browser does not support HTML5 video.
+        </video>
+      )}
     </div>
   );
 };
