bc01b9d90e51512ec3aa84691ef24872ff6fd96d
Chanikya5793 <148619135+Chanikya5793@users.noreply.github.com>
2024-02-20 06:36:33 +0000

    Update Sponsor section

--- app/components/(Sponsors)/Sponsors.tsx
+++ app/components/(Sponsors)/Sponsors.tsx
@@ -1,9 +1,11 @@
+import Leo from "@images/Leo.png";
+import CareerBaba from "@images/career.jpg";
+import Sahasra from "@images/sahasra.jpg";
 import Image from "next/image";
-
 
 const Sponsors = () => {
   return (
-    <section id="sponsors">
+    <section id="sponsors" className="mb-10">
       <h2 className="text-3xl font-bold text-center mt-20 mb-10">Sponsors</h2>
 {/*<Image
         src="/favicon.ico"
@@ -12,36 +14,40 @@ const Sponsors = () => {
         width={400}
         className="mx-auto mt-4 mb-4 "
   />*/}
-  <h4 className="text-xl font-bold text-center mb-10">Title Sponsor</h4>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+  <h4 className="text-xl font-bold text-center mb-8">Platinum Sponsor</h4>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-8 mb-10">
         <div className="flex flex-col items-center">
           <Image
-            src="/"
+            src={CareerBaba}
             alt="Sponsor"
             height={200}
             width={200}
-            className="mx-auto mt-4 mb-4 "
-          />
-        </div>
-        <div className="flex flex-col items-center">
-          <Image
-            src=""
-            alt="Sponsor"
-            height={200}
-            width={200}
-            className="mx-auto mt-4 mb-4 "
-          />
-        </div>
-        <div className="flex flex-col items-center">
-          <Image
-            src=""
-            alt="Sponsor"
-            height={200}
-            width={200}
-            className="mx-auto mt-4 mb-4 "
+            className="mx-auto"
           />
         </div>
       </div>
+      <h4 className="text-xl font-bold text-center">Silver Sponsor</h4>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+        <div className="flex flex-col items-center">
+          <Image
+            src={Leo}
+            alt="Sponsor"
+            height={200}
+            width={200}
+            className="mx-auto"
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <Image
+            src={Sahasra}
+            alt="Sponsor"
+            height={200}
+            width={200}
+            className="mx-auto"
+          />
+        </div>
+      </div>
+  
     </section>
   );
 };
