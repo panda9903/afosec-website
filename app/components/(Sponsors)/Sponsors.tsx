26a15dd86d1b37764e23fb78446c622c19c4ed9d
chanakya <218w1a05c1@vrsec.ac.in>
2024-02-23 16:17:11 +0530

    Update sponsor heading sizes

--- app/components/(Sponsors)/Sponsors.tsx
+++ app/components/(Sponsors)/Sponsors.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Sponsors = () => {
   return (
     <section id="sponsors" className="mb-10">
-      <h2 className="text-5xl font-bold text-center mt-20 mb-10">Sponsors</h2>
+      <h2 className="text-4xl font-bold text-center mt-20 mb-10">Sponsors</h2>
 {/*<Image
         src="/favicon.ico"
         alt="Picture of the author"
@@ -14,28 +14,28 @@ const Sponsors = () => {
         width={400}
         className="mx-auto mt-4 mb-4 "
   />*/}
-  <h4 className="text-4xl font-bold text-center mb-8">Platinum Sponsors</h4>
+  <h4 className="text-3xl font-bold text-center mb-8">Platinum Sponsors</h4>
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 mb-10">
         <div className="flex flex-col items-center">
           <Image
             src={CareerBaba}
             alt="Sponsor"
-            height={300}
-            width={300}
+            height={250}
+            width={250}
             className="mx-auto"
           />
         </div>
       </div>
       <br></br><br></br><br></br>
-      <h4 className="text-3xl font-bold text-center">Gold Sponsors</h4>
+      <h4 className="text-2xl font-bold text-center">Gold Sponsors</h4>
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 mb-10">
         <div className="flex flex-col items-center">
           {/*
           <Image
             src={Leo}
             alt="Sponsor"
-            height={400}
-            width={400}
+            height={200}
+            width={200}
             className="mx-auto"
           />
           */}
@@ -43,7 +43,7 @@ const Sponsors = () => {
         </div>
       </div>
       <br></br><br></br><br></br>
-      <h4 className="text-2xl font-bold text-center">Silver Sponsors</h4>
+      <h4 className="text-xl font-bold text-center">Silver Sponsors</h4>
       <div className="flex flex-col md:flex-row justify-center items-center gap-8">
         <div className="flex flex-col items-center">
           <Image
